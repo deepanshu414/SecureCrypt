622866512e87b5c8c6e65e04899d1e4a5305d775
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-08-01 14:28:53 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup
 setup(name="SecureCrypt",
       version="1.0.0",
       description="This script encrypts or decrypts files in a specified folder using a password, records the process in an Excel report, and handles errors during encryption and decryption.",
+      long_description=open('README.md').read(), 
+      long_description_content_type='text/markdown',
       author="Deepanshu antil",
       packages=['SecureCrypt'],
       author_email="deepanshuantil4113@gmail.com",
