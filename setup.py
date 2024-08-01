230ebadbbc8f157bb0e56c24e6012a134e69fb62
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-08-01 14:27:33 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="SecureCrypt",
+      version="1.0.0",
+      description="This script encrypts or decrypts files in a specified folder using a password, records the process in an Excel report, and handles errors during encryption and decryption.",
+      author="Deepanshu antil",
+      packages=['SecureCrypt'],
+      author_email="deepanshuantil4113@gmail.com",
+      url="https://github.com/deepanshu414",
+      license="MIT", 
+      install_requires=['pyAesCrypt','pandas','openpyxl'])
