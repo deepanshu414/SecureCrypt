b80f3c8e46d1e168a5d791baa222246be3599e16
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-08-05 16:30:18 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 setup(name="SecureCrypt",
-      version="1.0.0",
-      description="This script encrypts or decrypts files in a specified folder using a password, records the process in an Excel report, and handles errors during encryption and decryption.",
+      version="1.0.1",
+      description="SecureCrypt is a robust tool for encrypting and decrypting files in specified directories using AES encryption. It includes error handling and generates detailed Excel reports on the encryption and decryption processes.",
       long_description=open('README.md').read(), 
       long_description_content_type='text/markdown',
-      author="Deepanshu antil",
+       author="Deepanshu antil",
+      maintainer="Deepanshu antil",
       packages=['SecureCrypt'],
       author_email="deepanshuantil4113@gmail.com",
-      url="https://github.com/deepanshu414",
+      url="https://github.com/deepanshu414/SecureCrypt",
       license="MIT", 
       install_requires=['pyAesCrypt','pandas','openpyxl'])
