9727e28952cf944b3977ecad7c481f7cbde01071
Deepanshu <131351308+deepanshu414@users.noreply.github.com>
2024-08-06 07:24:27 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name="SecureCrypt",
-      version="1.0.1",
+      version="1.0.2",
       description="SecureCrypt is a robust tool for encrypting and decrypting files in specified directories using AES encryption. It includes error handling and generates detailed Excel reports on the encryption and decryption processes.",
       long_description=open('README.md').read(), 
       long_description_content_type='text/markdown',
